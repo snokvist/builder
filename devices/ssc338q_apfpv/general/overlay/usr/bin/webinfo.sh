e371c7bcde3afe049328458cb2aae9c1485709f9
Joakim <joakim.snokvist@gmail.com>
2025-07-13 18:08:50 +0200

    Create webinfo.sh

---
+++ devices/ssc338q_apfpv/general/overlay/usr/bin/webinfo.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+  
+echo "------- AP_FPV settings -------"
+echo "Current SSID: $(fw_printenv -n wlanssid || echo OpenIPC)"
+echo "Current Passw: $(fw_printenv -n wlanpass || echo 12345678)"
+echo "Current WlanPwr: $(fw_printenv -n wlanpwr || echo 250)"
+echo "Current Channel: $(fw_printenv -n wlanchan || echo 149)"
+echo "Current Mode: $(fw_printenv -n mode || echo manual)"
+echo "-------------------------------"
+echo "Connected sensor: $(ipcinfo -l)"
+echo "Currently set sensor: $(fw_printenv sensor)"
+echo "Current temp: $(ipcinfo -t)"
+echo "-------------------------------"
+
+exit 0
