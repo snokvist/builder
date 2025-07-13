b336ef9ee0ba9f4d28362498bd030a883101366b
Joakim <joakim.snokvist@gmail.com>
2025-07-13 11:05:27 +0200

    Update customizer.sh

--- devices/ssc338q_apfpv/general/overlay/usr/share/openipc/customizer.sh
+++ devices/ssc338q_apfpv/general/overlay/usr/share/openipc/customizer.sh
@@ -10,12 +10,15 @@ fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/o
 #
 # Set custom majestic settings
 #
-cli -s .isp.exposure 15
+cli -s .isp.exposure 8
 cli -s .video0.fps 60
+cli -s .video0.size 1440x1080
 cli -s .video0.bitrate 12000
 cli -s .video0.codec h265
-cli -s .video0.rcMode avbr
+cli -s .video0.rcMode cbr
 cli -s .outgoing.enabled true
-cli -s .outgoing.server udp://224.0.0.1:5600
+cli -s .outgoing.server udp://192.168.0.10:5600
 cli -s .records.split 5
 cli -s .records.notime true
+cli -s .fpv.enabled true
+cli -s .fpv.noiseLevel 1
