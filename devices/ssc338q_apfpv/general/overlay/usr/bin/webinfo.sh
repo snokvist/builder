0bd5e962f852f37409388390fb2227ae72b116c1
Joakim <joakim.snokvist@gmail.com>
2025-07-16 21:29:18 +0200

    Update webinfo.sh

--- devices/ssc338q_apfpv/general/overlay/usr/bin/webinfo.sh
+++ devices/ssc338q_apfpv/general/overlay/usr/bin/webinfo.sh
@@ -7,6 +7,7 @@ echo "Current WlanPwr: $(fw_printenv -n wlanpwr || echo 250)"
 echo "Current Channel: $(fw_printenv -n wlanchan || echo 149)"
 echo "Current Mode: $(fw_printenv -n mode || echo manual)"
 echo "Current MSP TTY: $(fw_printenv -n msposd_tty || echo -)"
+echo "Current Output: $(fw_printenv -n master_protocol || echo udp)://$(fw_printenv -n master_ip || echo 192.168.0.10):$(fw_printenv -n master_port || echo 5600)
 echo "-------------------------------"
 echo "Connected sensor: $(ipcinfo -l)"
 echo "Currently set sensor: $(fw_printenv sensor)"
