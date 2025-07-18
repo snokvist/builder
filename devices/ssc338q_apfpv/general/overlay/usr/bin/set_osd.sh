bbc93469babb78940d7cdf87dc502d3064e1ab61
Joakim <joakim.snokvist@gmail.com>
2025-07-18 09:51:09 +0200

    Create set_osd.sh

---
+++ devices/ssc338q_apfpv/general/overlay/usr/bin/set_osd.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+case "$1" in
+  mode-manual)
+    echo "Setting link control to manual." | tee /tmp/webui.log
+    fw_setenv mode "manual"
+    ;;
+  
+  mode-aalink)
+    echo "Setting link control to aalink." | tee /tmp/webui.log
+    fw_setenv mode "aalink"
+    ;;
+
+  mode-tty0)
+    echo "Setting tty to standalone." | tee /tmp/webui.log
+    fw_setenv msposd_tty "standalone"
+    ;;
+
+  mode-tty1)
+    echo "Setting tty to /dev/ttyS0." | tee /tmp/webui.log
+    fw_setenv msposd_tty "/dev/ttyS0"
+    ;;
+
+  mode-tty2)
+    echo "Setting tty to /dev/ttyS1." | tee /tmp/webui.log
+    fw_setenv msposd_tty "/dev/ttyS1"
+    ;;
+
+  mode-tty3)
+    echo "Setting tty to /dev/ttyS2." | tee /tmp/webui.log
+    fw_setenv msposd_tty "/dev/ttyS2"
+    ;;
+
+  *)
+    echo "Invalid argument: $1" | tee /tmp/webui.log
+    exit 1
+    ;;
+esac
+
+/etc/init.d/S99msposd start
+/etc/init.d/S991aalink start
+
+exit 0
